cc28df6f75487337429250d5ffe0e773bca49686
MDaryaK <dasha-41@mail.ru>
2023-08-19 02:18:38 +0300

    ПР13

--- src/models/card.js
+++ src/models/card.js
@@ -19,12 +19,6 @@ const cardSchema = new mongoose.Schema({
   link: {
     type: String,
     required: true,
-    alidate: {
-      validator(url) {
-        return /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&\/=]*)$/.test(url);
-      },
-      message: 'Введите URL',
-    },
   },
   createdAt: {
     type: Date,
